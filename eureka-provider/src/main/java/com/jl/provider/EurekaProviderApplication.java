9e45190f98973c00c6e60da1ae7e7b62ab978bfd
ldy <506508008@qq.com>
2021-03-11 16:11:49 +0800

    Zuul

--- eureka-provider/src/main/java/com/jl/provider/EurekaProviderApplication.java
+++ eureka-provider/src/main/java/com/jl/provider/EurekaProviderApplication.java
@@ -1,11 +1,13 @@
 package com.jl.provider;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableAdminServer
 public class EurekaProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaProviderApplication.class, args);
---
+++ zuul-test/src/test/java/com/jl/zuultest/ZuulTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jl.zuultest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
